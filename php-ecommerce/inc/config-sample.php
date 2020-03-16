4fe0562416b4a393ac5447d469e0fd8517a8c6d0
Francesco Formica <vormix90@gmail.com>
2020-03-16 12:52:47 +0100

    added config-sample.php

---
+++ php-ecommerce/inc/config-sample.php
@@ -0,0 +1,10 @@
+<?php
+
+define('ROOT_URL', 'http://localhost/php-ecommerce/');
+define('ROOT_PATH', 'C:\\xampp\\htdocs\\php-ecommerce\\');
+define('SITE_NAME', 'PHP-Ecommerce');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'php-ecommerce');
