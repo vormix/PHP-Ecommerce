f67fe9f826e54a694fac482d87072c513ee7925a
Francesco Formica <vormix90@gmail.com>
2020-03-16 12:41:58 +0100

    updated sql script

--- phpecommerce-db-script.sql
+++ phpecommerce-db-script.sql
@@ -1,26 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Creato il: Feb 12, 2020 alle 08:02
--- Versione del server: 10.1.36-MariaDB
--- Versione PHP: 7.2.10
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `website1`
---
 
 DELIMITER $$
 --
@@ -43,7 +27,7 @@ CREATE  PROCEDURE `all_orders` (`status_code` VARCHAR(10))  BEGIN
     o.created_at DESC;
 END$$
 
-CREATE  PROCEDURE `cart_items` (`cart_identifier` INT)  BEGIN
+CREATE  PROCEDURE `cart_items` (IN `cart_identifier` INT)  BEGIN
   SELECT 
     c.id as cart_id
         , ci.id as cart_item_id
@@ -51,8 +35,12 @@ CREATE  PROCEDURE `cart_items` (`cart_identifier` INT)  BEGIN
         , p.id as product_id
         , p.description as product_description
         , ifnull(ci.quantity, 0) as quantity
-        , ifnull(p.price, 0) as single_price
-        , ifnull(ci.quantity,0) * ifnull(p.price, 0) as total_price
+        ,IF(p.`sconto`>0 AND p.`data_inizio_sconto` <= DATE(NOW()) AND p.`data_fine_sconto` >= DATE(NOW()),
+            CAST((p.`price` -(p.`price`*p.`sconto`)/100) AS DECIMAL(8,2)) 
+          ,ifnull(p.`price`, 0))AS single_price
+        , ifnull(ci.quantity,0) * IF(p.`sconto`>0 AND `data_inizio_sconto` <= DATE(NOW()) AND `data_fine_sconto` >= DATE(NOW()),
+           CAST((`price` -(`price`*`sconto`)/100) AS DECIMAL(8,2)) 
+          ,ifnull(`price`, 0)) as total_price
     FROM
     cart as c
         INNER JOIN cart_item as ci
@@ -183,7 +171,7 @@ CREATE TABLE `address` (
 
 INSERT INTO `address` (`id`, `user_id`, `street`, `city`, `cap`) VALUES
 (2, 1, 'Via Admin 1', 'Roma', '00100'),
-(3, 2, 'Via Regular 2', 'Roma', '00100');
+(3, 2, 'Via Regular 2', 'Roma', '00100')
 
 -- --------------------------------------------------------
 
@@ -196,12 +184,6 @@ CREATE TABLE `cart` (
   `user_id` int(11) NOT NULL,
   `client_id` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dump dei dati per la tabella `cart`
---
-
--- INSERT INTO `cart` (`id`, `user_id`, `client_id`) VALUES
 
 
 
@@ -218,14 +200,6 @@ CREATE TABLE `cart_item` (
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dump dei dati per la tabella `cart_item`
---
-
-INSERT INTO `cart_item` (`id`, `cart_id`, `product_id`, `quantity`) VALUES
-(48, 19, 6, 3),
-(49, 19, 14, 2),
-(57, 25, 14, 20);
 
 -- --------------------------------------------------------
 
@@ -241,14 +215,6 @@ CREATE TABLE `orders` (
   `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dump dei dati per la tabella `orders`
---
-
-INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`, `status`) VALUES
-(4, 1, '2019-05-02 12:21:15', NULL, 'pending'),
-(12, 2, '2019-05-02 14:20:31', NULL, 'pending'),
-(13, 1, '2019-05-02 15:39:08', '2019-05-02 20:39:59', 'shipped');
 
 -- --------------------------------------------------------
 
@@ -263,18 +229,7 @@ CREATE TABLE `order_item` (
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dump dei dati per la tabella `order_item`
---
 
-INSERT INTO `order_item` (`id`, `order_id`, `product_id`, `quantity`) VALUES
-(70, 4, 6, 10),
-(71, 4, 14, 9),
-(100, 12, 6, 9),
-(101, 12, 26, 6),
-(102, 12, 33, 1),
-(103, 12, 14, 2),
-(107, 13, 14, 2);
 
 -- --------------------------------------------------------
 
@@ -284,27 +239,48 @@ INSERT INTO `order_item` (`id`, `order_id`, `product_id`, `quantity`) VALUES
 
 CREATE TABLE `product` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `description` text CHARACTER SET latin1 NOT NULL,
+  `name` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `description` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `category_id` int(11) NOT NULL,
-  `price` decimal(10,2) NOT NULL DEFAULT '0.00'
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `sconto` int(9) NOT NULL DEFAULT '0',
+  `data_inizio_sconto` date DEFAULT NULL,
+  `data_fine_sconto` date DEFAULT NULL,
+  `qta` int(9) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Dump dei dati per la tabella `product`
 --
 
-INSERT INTO `product` (`id`, `name`, `description`, `category_id`, `price`) VALUES
-(6, 'Prodotto 1', 'Questo Ã¨ il prodotto numero 1\r\n\r\nLorem ipsum dolor sit, amet consectetur adipisicing elit. Quod quasi, illum esse obcaecati quisquam asperiores nemo eaque optio aliquid corporis soluta harum ad numquam. Exercitationem vero enim doloribus optio dolor?\r\n\r\nLorem ipsum dolor sit, amet consectetur adipisicing elit. Quod quasi, illum esse obcaecati quisquam asperiores nemo eaque optio aliquid corporis soluta harum ad numquam. Exercitationem vero enim doloribus optio dolor?', 1, '3.90'),
-(14, 'Prodotto 7', 'Questo Ã¨ il prodotto numero 7\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Animi ipsam cum inventore dignissimos quisquam, earum omnis excepturi accusantium incidunt dicta veritatis, amet commodi atque saepe, laborum dolorem voluptatibus aliquid illo?\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Animi ipsam cum inventore dignissimos quisquam, earum omnis excepturi accusantium incidunt dicta veritatis, amet commodi atque saepe, laborum dolorem voluptatibus aliquid illo?', 1, '8.25'),
-(26, 'Prodotto 8', 'Questo Ã¨ il prodotto n. 8\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Animi ipsam cum inventore dignissimos quisquam, earum omnis excepturi accusantium incidunt dicta veritatis, amet commodi atque saepe, laborum dolorem voluptatibus aliquid illo?\r\n\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Animi ipsam cum inventore dignissimos quisquam, earum omnis excepturi accusantium incidunt dicta veritatis, amet commodi atque saepe, laborum dolorem voluptatibus aliquid illo?', 1, '5.90'),
-(33, 'Prodotto 9', 'questo Ã¨ il prodotto numero 9+', 2, '3.50'),
-(34, 'Prodotto 10', 'Questo Ã¨ il prodotto numero 10\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Soluta tempore asperiores assumenda laborum, reprehenderit repellat suscipit eligendi officia ea saepe praesentium nisi alias porro quas sint maiores recusandae, perferendis omnis.', 1, '10.00'),
-(35, 'Prodotto 11', 'Questo Ã¨ il prodotto numero 10\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Soluta tempore asperiores assumenda laborum, reprehenderit repellat suscipit eligendi officia ea saepe praesentium nisi alias porro quas sint maiores recusandae, perferendis omnis.', 1, '5.00'),
-(36, 'Prodotto 12', 'Questo Ã¨ il prodotto numero 12\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Soluta tempore asperiores assumenda laborum, reprehenderit repellat suscipit eligendi officia ea saepe praesentium nisi alias porro quas sint maiores recusandae, perferendis omnis.', 2, '8.00'),
-(37, 'Prodotto 13', 'Questo Ã¨ il prodotto numero 13\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Soluta tempore asperiores assumenda laborum, reprehenderit repellat suscipit eligendi officia ea saepe praesentium nisi alias porro quas sint maiores recusandae, perferendis omnis.', 2, '9.99'),
-(38, 'Prodotto 14', 'Questo Ã¨ il prodotto numero 14\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Soluta tempore asperiores assumenda laborum, reprehenderit repellat suscipit eligendi officia ea saepe praesentium nisi alias porro quas sint maiores recusandae, perferendis omnis.', 2, '3.99'),
-(39, 'Nuovo prodotto Figo', 'molto interessante per tutti', 1, '0.50');
+INSERT INTO `product` (`id`, `name`, `description`, `category_id`, `price`, `sconto`, `data_inizio_sconto`, `data_fine_sconto`, `qta`) VALUES
+(6, 'Samsung A10 Black', 'Questo Ã¨ il prodotto numero 1 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod quasi, illum esse obcaecati quisquam asperiores nemo eaque optio aliquid corporis soluta harum ad numquam. Exercitationem vero enim doloribus optio dolor? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod quasi, illum esse obcaecati quisquam asperiores nemo eaque optio aliquid corporis soluta harum ad numquam. Exercitationem vero enim doloribus optio dolor?', 2, '3.90', 16, '2020-03-09', '2020-03-13', 0),
+(14, 'Prodotto 7', 'Questo Ã¨ il prodotto numero 7\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Animi ipsam cum inventore dignissimos quisquam, earum omnis excepturi accusantium incidunt dicta veritatis, amet commodi atque saepe, laborum dolorem voluptatibus aliquid illo?\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Animi ipsam cum inventore dignissimos quisquam, earum omnis excepturi accusantium incidunt dicta veritatis, amet commodi atque saepe, laborum dolorem voluptatibus aliquid illo?', 1, '8.25', 20, '2020-03-06', '2020-05-16', 0),
+(26, 'Prodotto 8', 'Questo Ã¨ il prodotto n. 8\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Animi ipsam cum inventore dignissimos quisquam, earum omnis excepturi accusantium incidunt dicta veritatis, amet commodi atque saepe, laborum dolorem voluptatibus aliquid illo?\r\n\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Animi ipsam cum inventore dignissimos quisquam, earum omnis excepturi accusantium incidunt dicta veritatis, amet commodi atque saepe, laborum dolorem voluptatibus aliquid illo?', 1, '5.90', 0, NULL, NULL, 0),
+(33, 'Prodotto 9', 'questo Ã¨ il prodotto numero 9+', 2, '3.50', 0, NULL, NULL, 0),
+(34, 'Prodotto 10', 'Questo Ã¨ il prodotto numero 10\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Soluta tempore asperiores assumenda laborum, reprehenderit repellat suscipit eligendi officia ea saepe praesentium nisi alias porro quas sint maiores recusandae, perferendis omnis.', 1, '10.00', 0, NULL, NULL, 0),
+(35, 'Prodotto 11', 'Questo Ã¨ il prodotto numero 10\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Soluta tempore asperiores assumenda laborum, reprehenderit repellat suscipit eligendi officia ea saepe praesentium nisi alias porro quas sint maiores recusandae, perferendis omnis.', 1, '5.00', 0, NULL, NULL, 0),
+(36, 'Prodotto 12', 'Questo Ã¨ il prodotto numero 12\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Soluta tempore asperiores assumenda laborum, reprehenderit repellat suscipit eligendi officia ea saepe praesentium nisi alias porro quas sint maiores recusandae, perferendis omnis.', 2, '8.00', 0, NULL, NULL, 0),
+(37, 'Prodotto 13', 'Questo Ã¨ il prodotto numero 13\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Soluta tempore asperiores assumenda laborum, reprehenderit repellat suscipit eligendi officia ea saepe praesentium nisi alias porro quas sint maiores recusandae, perferendis omnis.', 2, '9.99', 0, NULL, NULL, 0),
+(38, 'Prodotto 14', 'Questo Ã¨ il prodotto numero 14\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Soluta tempore asperiores assumenda laborum, reprehenderit repellat suscipit eligendi officia ea saepe praesentium nisi alias porro quas sint maiores recusandae, perferendis omnis.', 2, '3.99', 0, NULL, NULL, 0),
+
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `product_images`
+--
+
+CREATE TABLE `product_images` (
+  `id` int(10) NOT NULL,
+  `product_id` int(10) NOT NULL,
+  `image_extension` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `product_images`
+--
+
 
 -- --------------------------------------------------------
 
@@ -319,7 +295,7 @@ CREATE TABLE `user` (
   `email` varchar(100) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_type` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -328,7 +304,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `created_at`, `user_type`, `password`) VALUES
 (1, 'Amministratore', 'Di Sistema', 'admin@email.com', '2019-04-26 21:26:37', 'admin', 'password'),
-(2, 'Regolare', 'Utente', 'regular@email.com', '2019-05-02 16:34:56', 'regular', 'password');
+(2, 'Regolare', 'Utente', 'regular@email.com', '2019-05-02 16:34:56', 'regular', 'password')
 
 --
 -- Indici per le tabelle scaricate
@@ -371,6 +347,13 @@ ALTER TABLE `product`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indici per le tabelle `product_images`
+--
+ALTER TABLE `product_images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_Product_Image` (`product_id`);
+
+--
 -- Indici per le tabelle `user`
 --
 ALTER TABLE `user`
@@ -384,19 +367,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT per la tabella `address`
 --
 ALTER TABLE `address`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT per la tabella `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- AUTO_INCREMENT per la tabella `cart_item`
 --
 ALTER TABLE `cart_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
 
 --
 -- AUTO_INCREMENT per la tabella `orders`
@@ -414,15 +397,22 @@ ALTER TABLE `order_item`
 -- AUTO_INCREMENT per la tabella `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+
+--
+-- AUTO_INCREMENT per la tabella `product_images`
+--
+ALTER TABLE `product_images`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT per la tabella `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+
+ALTER TABLE `product_images`
+  ADD CONSTRAINT `FK_Product_Image` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
