e5b6d7c8430cbc2b7c88546661d44690655ea3cd
Francesco Formica <vormix90@gmail.com>
2020-03-18 17:58:41 +0100

    fix issue #10

--- php-ecommerce/api/shop/cart.php
+++ php-ecommerce/api/shop/cart.php
@@ -2,32 +2,28 @@
 require_once '../../inc/init.php'; 
 
 if (! defined('ROOT_URL')) {
-    die;
-  }
+  die;
+}
 
-  // Instanzio Cart Manager
-  $cm = new CartManager();
-  $cartId = $cm->getCurrentCartId();
-  $cart_id = esc($_POST['cart_id']);
-  $product_id = esc($_POST['product_id']);
-  $pm = new ProductManager();
-  $product=$pm->get($product_id);
-  if($product->qta > 1) {  
+// Instanzio Cart Manager
+$cm = new CartManager();
+$cartId = $cm->getCurrentCartId();
+$cart_id = esc($_POST['cart_id']);
+$product_id = esc($_POST['product_id']);
+$pm = new ProductManager();
+$product=$pm->get($product_id);
     
-      if (isset($_POST['minus'])){
-        $cm->removeFromCart($product_id, $cart_id);
-        $pm->increaseQuantity($product_id);
-      }
-      if (isset($_POST['plus'])){
-        $cm->addToCart($product_id, $cart_id);
-        $pm->decreaseQuantity($product_id);
-      }
+if (isset($_POST['minus'])){
+  $cm->removeFromCart($product_id, $cart_id);
+  $pm->increaseQuantity($product_id);
+}
+else if (isset($_POST['plus']) && $product->qta > 1){
+  $cm->addToCart($product_id, $cart_id);
+  $pm->decreaseQuantity($product_id);
+}
 
-  }
-  $cart_total = $cm->getCartTotal($cartId);
-  $cart_items = $cm->getCartItems($cartId);
-
-
+$cart_total = $cm->getCartTotal($cartId);
+$cart_items = $cm->getCartItems($cartId);
 
 header('Content-type: application/json');
 $array=[
