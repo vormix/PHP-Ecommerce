4144ac55dea25578f1b095c802d347427c3cf433
Francesco Formica <vormix90@gmail.com>
2021-03-17 10:10:50 +0100

    aggiunto config-sample.php e rimosso config.php

---
+++ php-ecommerce/inc/config-sample.php
@@ -0,0 +1,9 @@
+<?php
+
+define('ROOT_URL', 'http://localhost/PHP-Ecommerce/php-ecommerce/');
+define('ROOT_PATH', 'C:\\xampp\\htdocs\\PHP-Ecommerce\\php-ecommerce\\');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpecommerce');
--- php-ecommerce/inc/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('ROOT_URL', 'http://localhost:8012/Github/YouTubeSeries/PHP-Ecommerce/php-ecommerce/');
-define('ROOT_PATH', 'C:\\software\\apache\\htdocs\\Github\\YouTubeSeries\\PHP-Ecommerce\\php-ecommerce\\');
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'Password1');
-define('DB_NAME', 'phpecommerce_youtubeseries');
